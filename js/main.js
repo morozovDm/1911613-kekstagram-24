8d730a7fe5c387e9751f3d5328486ea9ac214ff7
morozovDm <mrrais@yandex.ru>
2021-10-25 11:26:17 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const defaultComments = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 function randomInteger(min, max) {
@@ -40,7 +39,7 @@ function generatePhotoItems() {
       url: `photos/${id}.jpg`,
       description: 'random desc',
       likes: randomInteger(15, 200),
-      comments: generatePhotoComments(randomInteger(5, 25), defaultComments)
+      comments: generatePhotoComments(randomInteger(5, 25), defaultComments),
     });
   }
   return photoItems;
@@ -48,4 +47,4 @@ function generatePhotoItems() {
 
 randomInteger(0, 1);
 checkStrMaxLength('str', 3);
-console.log(generatePhotoItems());
+generatePhotoItems();
