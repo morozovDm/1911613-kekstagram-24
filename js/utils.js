d06a8f6ec96a69847df3e707f97876d6fe911b49
morozovDm <mrrais@yandex.ru>
2021-10-26 12:46:03 +0400

    Модуляция

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+export function randomInteger(min, max) {
+  if(min < max) {
+    const rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+  alert('некорректный диапазон значений'); // eslint-disable-line no-alert
+}
+
+export function checkStrMaxLength(str, maxLength) {
+  return !!str && str.length <= maxLength;
+}
