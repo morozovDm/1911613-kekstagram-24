e55ba9c961b1031a20dcf3aba1b123382e2775ad
morozovDm <mrrais@yandex.ru>
2021-11-08 16:47:05 +0400

    Отрисуй меня полностью (часть 2)

--- js/photo-card.js
+++ js/photo-card.js
@@ -1,3 +1,4 @@
+import * as photoPreview from './photo-preview.js';
 
 export function generateCards(data) {
   const picturesSection = document.getElementsByClassName('pictures')[0];
@@ -5,9 +6,13 @@ export function generateCards(data) {
   if(data) {
     data.forEach((item) => {
       const template = document.querySelector('#picture').cloneNode(true).content;
-      template.querySelector('img').src = item.url;
+      const img = template.querySelector('img');
+      img.src = item.url;
       template.querySelector('.picture__likes').textContent = item.likes;
       template.querySelector('.picture__comments').textContent = item.comments.length;
+      img.addEventListener('click', () => {
+        photoPreview.openPreviewViewer(item);
+      });
       fragment.appendChild(template);
     });
     picturesSection.appendChild(fragment);
---
+++ js/photo-preview.js
@@ -0,0 +1,50 @@
+export function openPreviewViewer(data) {
+  const previewViewerNode = document.querySelector('.big-picture');
+  const closeButton = previewViewerNode.querySelector('.big-picture__cancel');
+  updateParams(previewViewerNode, data);
+  previewViewerNode.classList.remove('hidden');
+  document.querySelector('body').classList.add('.modal-open');
+  closeButton.addEventListener('click', () => {
+    closePreviewViewer(previewViewerNode);
+    closeButton.removeEventListener('click', () => {});
+  });
+  document.addEventListener('keydown', ({key}) => {
+    if (key === 'Escape') {
+      closePreviewViewer(previewViewerNode);
+      document.removeEventListener('keydown', () => {});
+    }
+  });
+}
+
+export function closePreviewViewer(node) {
+  node.classList.add('hidden');
+  document.querySelector('body').classList.remove('.modal-open');
+}
+
+function updateParams(node, data) {
+  const img = node.querySelector('.big-picture__img img');
+  img.src = data.url;
+  node.querySelector('.likes-count').textContent = data.likes;
+  node.querySelector('.comments-count').textContent = data.comments.length;
+  node.querySelector('.social__caption').textContent = data.description;
+  updateComments(node.querySelector('.social__comments'), data.comments);
+  node.querySelector('.social__comment-count').classList.add('hidden');
+  node.querySelector('.comments-loader').classList.add('hidden');
+  node.classList.remove('hidden');
+}
+
+function updateComments(node, newComments) {
+  const fragment = document.createDocumentFragment();
+  while(node.children.length > 0) {
+    node.removeChild(node.lastChild);
+  }
+  newComments.forEach((comment) => {
+    const commentTemplate = document.getElementById('comment-item').cloneNode(true).content;
+    const img = commentTemplate.querySelector('img');
+    img.src = comment.avatar;
+    img.alt = comment.name;
+    commentTemplate.querySelector('.social__text').textContent = comment.message;
+    fragment.appendChild(commentTemplate);
+  });
+  node.appendChild(fragment);
+}
