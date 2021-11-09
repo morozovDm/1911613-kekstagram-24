e123bff279f09d22f0ff4731543001a7a8c21340
morozovDm <mrrais@yandex.ru>
2021-11-09 14:00:28 +0400

    Правда или действие (часть 2)

--- js/photo-preview.js
+++ js/photo-preview.js
@@ -1,52 +1,68 @@
 import * as utils from './utils.js';
 
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+const commentsLoader = document.querySelector('.comments-loader');
+let visibleCommentsCount = 5;
+let loadNewCommentsCallbackWrapper;
+
+bigPictureCancel.addEventListener('click', () => closePreviewViewer(bigPicture));
+document.addEventListener('keydown', ({key}) => {
+  if (key === 'Escape') {
+    closePreviewViewer(bigPicture);
+  }
+});
+
 export function openPreviewViewer(data) {
-  const previewViewerNode = document.querySelector('.big-picture');
-  const closeButton = previewViewerNode.querySelector('.big-picture__cancel');
-  updateParams(previewViewerNode, data);
-  previewViewerNode.classList.remove('hidden');
+  updateParams(bigPicture, data);
+  bigPicture.classList.remove('hidden');
   utils.toggleBodyModalOpen();
-  closeButton.addEventListener('click', () => {
-    closePreviewViewer(previewViewerNode);
-    closeButton.removeEventListener('click', () => {});
-  });
-  document.addEventListener('keydown', ({key}) => {
-    if (key === 'Escape') {
-      closePreviewViewer(previewViewerNode);
-      document.removeEventListener('keydown', () => {});
-    }
-  });
+  loadNewCommentsCallbackWrapper = loadNewComments.bind(this, bigPicture, data.comments);
+  commentsLoader.addEventListener('click', loadNewCommentsCallbackWrapper);
 }
 
 export function closePreviewViewer(node) {
   node.classList.add('hidden');
   utils.toggleBodyModalOpen();
+  visibleCommentsCount = 5;
+  commentsLoader.removeEventListener('click', loadNewCommentsCallbackWrapper);
 }
 
 function updateParams(node, data) {
   const img = node.querySelector('.big-picture__img img');
   img.src = data.url;
   node.querySelector('.likes-count').textContent = data.likes;
-  node.querySelector('.comments-count').textContent = data.comments.length;
+  node.querySelector('.comments-count.current').textContent = visibleCommentsCount;
+  node.querySelector('.comments-count.total').textContent = data.comments.length;
   node.querySelector('.social__caption').textContent = data.description;
-  updateComments(node.querySelector('.social__comments'), data.comments);
-  node.querySelector('.social__comment-count').classList.add('hidden');
-  node.querySelector('.comments-loader').classList.add('hidden');
+  updateComments(node.querySelector('.social__comments'), data.comments, visibleCommentsCount);
+  document.querySelector('.comments-loader').classList.remove('hidden');
   node.classList.remove('hidden');
 }
 
-function updateComments(node, newComments) {
+function updateComments(node, newComments, commentsCount) {
   const fragment = document.createDocumentFragment();
   while(node.children.length > 0) {
     node.removeChild(node.lastChild);
   }
-  newComments.forEach((comment) => {
-    const commentTemplate = document.getElementById('comment-item').cloneNode(true).content;
-    const img = commentTemplate.querySelector('img');
-    img.src = comment.avatar;
-    img.alt = comment.name;
-    commentTemplate.querySelector('.social__text').textContent = comment.message;
-    fragment.appendChild(commentTemplate);
+  newComments.forEach((comment, index) => {
+    if(index < commentsCount) {
+      const commentTemplate = document.getElementById('comment-item').cloneNode(true).content;
+      const img = commentTemplate.querySelector('img');
+      img.src = comment.avatar;
+      img.alt = comment.name;
+      commentTemplate.querySelector('.social__text').textContent = comment.message;
+      fragment.appendChild(commentTemplate);
+    }
   });
   node.appendChild(fragment);
+}
+
+function loadNewComments(node, comments) {
+  visibleCommentsCount = visibleCommentsCount + 5 < comments.length ? visibleCommentsCount + 5 : comments.length;
+  node.querySelector('.comments-count.current').textContent = visibleCommentsCount;
+  updateComments(node.querySelector('.social__comments'), comments, visibleCommentsCount);
+  if(visibleCommentsCount === comments.length) {
+    document.querySelector('.comments-loader').classList.add('hidden');
+  }
 }
